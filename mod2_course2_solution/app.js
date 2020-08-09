53eeed72b1ca259a1f112e4b74d293711a7ce638
Rudel <rudel.khayrullin@mail.ru>
2020-08-09 14:59:10 +0300

    mod 2

---
+++ mod2_course2_solution/app.js
@@ -0,0 +1,68 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+	// body...
+  var bueList = this;
+
+  bueList.items = ShoppingListCheckOffService.getBueItems();
+
+  bueList.toBue = function (itemIndex) {
+	ShoppingListCheckOffService.toBue(itemIndex);  	
+  }
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+	// body...
+  var boughtList = this;
+
+  boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+
+
+
+function ShoppingListCheckOffService()
+{
+	var service = this;
+
+	var bueItems=[{
+      name: "cookies",
+      quantity: 10
+    },
+    {
+      name: "eggs",
+      quantity: 20
+    },
+    {
+      name: "crisps",
+      quantity: 2
+    }];
+
+	var boughtItems=[];
+
+	service.getBueItems = function () {
+		return bueItems;
+	}
+
+	service.getBoughtItems = function () {
+		return boughtItems;
+	}
+
+	service.toBue = function (itemIndex) {
+		var redItem = bueItems[itemIndex]; 
+		bueItems.splice(itemIndex, 1);
+		boughtItems.push(redItem);		
+	}
+}
+
+})();
+
